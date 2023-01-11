05138d25fb68415e70b0abf5b62d990e3e99d6e5
Patryk Grodon <kyrtapek1@gmail.com>
2023-01-11 16:40:00 +0100

    Fill table with dummy data

--- src/modules/grades/components/GradesTable/GradesTable.tsx
+++ src/modules/grades/components/GradesTable/GradesTable.tsx
@@ -7,6 +7,29 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import { lessonPlan } from "modules/lessonPlan/constants";
+
+const headers = [
+  "Przedmiot",
+  "I Semestr",
+  "II Semestr",
+  "Średnia",
+  "Ocena śródroczna",
+  "Ocena końcowa",
+];
+
+const getAllSubjects = () => {
+  const keys = Object.keys(lessonPlan) as (keyof typeof lessonPlan)[];
+  const subjects: string[] = [];
+  keys.forEach((key) => {
+    lessonPlan[key].forEach((subject) => {
+      if (subjects.includes(subject) || subject === "") return;
+      subjects.push(subject);
+    });
+  });
+
+  return subjects;
+};
 
 const GradesTable = () => {
   return (
@@ -15,13 +38,22 @@ const GradesTable = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Przedmiot</TableCell>
+              {headers.map((header) => (
+                <TableCell key={header}>{header}</TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>język polski</TableCell>
-            </TableRow>
+            {getAllSubjects().map((subject) => (
+              <TableRow key={subject}>
+                <TableCell>{subject}</TableCell>
+                <TableCell>4, 3, 2, 1</TableCell>
+                <TableCell>4, 3, 2, 1</TableCell>
+                <TableCell>2.5</TableCell>
+                <TableCell>2.5</TableCell>
+                <TableCell>2.5</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </Paper>
