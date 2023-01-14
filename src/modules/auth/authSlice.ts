f2f6d1f0ba624f72c35521e6deb8894616da9e2d
Patryk Grodon <kyrtapek1@gmail.com>
2023-01-14 12:11:50 +0100

    Change default app port, add getAccType to handle different account types

--- src/modules/auth/authSlice.ts
+++ src/modules/auth/authSlice.ts
@@ -20,12 +20,24 @@ const initialState: AuthState = {
   user: undefined,
   isLoggingIn: false,
 };
+const getAccType = (email: string): AccountType => {
+  switch (email) {
+    case "test1@gmail.com":
+      return "admin";
+    case "test2@gmail.com":
+      return "teacher";
+    case "test3@gmail.com":
+      return "student";
+    default:
+      return "student";
+  }
+};
 
 export const login = createAsyncThunk(
   "login",
   async ({ email }: LoginFormValues) => {
     await sleep(500);
-    return { id: 1, email, accountType: "student" } as const;
+    return { id: 1, email, accountType: getAccType(email) } as const;
   }
 );
 export const createAdminAccount = createAsyncThunk(
