67e0c1e53e9f414a22285c01523cf3f3bde96cc0
Patryk Grodon <kyrtapek1@gmail.com>
2023-01-14 13:27:17 +0100

    From now on navigate route in RestrictedRoutes depend on account type

--- src/routes/RestrictedRoutes.tsx
+++ src/routes/RestrictedRoutes.tsx
@@ -1,18 +1,32 @@
 import { useAppSelector } from "app/hooks";
+import { AccountType } from "modules/auth/authSlice";
 import AuthLayout from "modules/auth/components/AuthLayout";
 import { Navigate, Outlet } from "react-router-dom";
 import routes from "./routePaths";
 
+const getCorrectNavigateRoute = (accountType: AccountType) => {
+  switch (accountType) {
+    case "admin":
+      return routes.Classes;
+    case "student":
+      return routes.LessonPlan;
+    case "teacher":
+      return routes.LessonPlan;
+  }
+};
+
 const RestrictedRoutes = () => {
   const { user } = useAppSelector(({ auth }) => auth);
   const isRestricted = !user;
-  return isRestricted ? (
-    <AuthLayout>
-      <Outlet />
-    </AuthLayout>
-  ) : (
-    <Navigate to={routes.LessonPlan} replace />
-  );
+
+  if (isRestricted)
+    return (
+      <AuthLayout>
+        <Outlet />
+      </AuthLayout>
+    );
+
+  return <Navigate to={getCorrectNavigateRoute(user.accountType)} replace />;
 };
 
 export default RestrictedRoutes;
